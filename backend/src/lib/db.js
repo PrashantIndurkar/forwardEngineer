ba4ac0e8cbebf65db54445de586be8cb271d94e5
prashant indurkar <prashantri2000@gmail.com>
2026-06-06 09:53:12 +0530

    feat: implement MongoDB connection for backend and integrate Clerk authentication in frontend

---
+++ backend/src/lib/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { ENV } from "./env.js";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(ENV.DB_URL);
+    console.log("✅ Connected MongoDB successfully: ", conn.connection.host);
+  } catch (error) {
+    console.error("❌ Error connecting to MongoDB:   ", error);
+    // 0 means success, 1 means failed
+    process.exit(1);
+  }
+};
