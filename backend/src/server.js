6b70c8a2ee07afbbd4fc957a3670badda021c77b
prashant indurkar <prashantri2000@gmail.com>
2026-06-07 01:45:26 +0530

    feat: register Inngest functions in the server middleware and fix typo

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,17 +3,16 @@ import { ENV } from "./lib/env.js";
 import path from "path";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
-import { inngest } from "./lib/inngest.js";
+import { inngest, functions  } from "./lib/inngest.js";
 
 const app = express();
 
 const __dirname = path.resolve();
 
-// Middelware
+// Middleware
 app.use(express.json());
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
-app.use("/api/inngest", serve({client: inngest,functions}));
-
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res) => {
   res.status(200).json({
