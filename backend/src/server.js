751600b19ed068d34ef8a2f0675d164a2180fa0d
prashant indurkar <prashantri2000@gmail.com>
2026-06-07 01:54:38 +0530

    refactor: update inngest imports to use local library configuration and serve middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,8 @@ import { ENV } from "./lib/env.js";
 import path from "path";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
-import { inngest, functions } from "inngest/express";
+import { inngest, functions } from "./lib/inngest.js";
+import { serve } from "inngest/express";
 
 const app = express();
 
