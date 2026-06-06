ee9dcd95fbc21d4c14d3644e06adce30ec789c75
prashant indurkar <prashantri2000@gmail.com>
2026-06-06 23:07:19 +0530

    feat: implement user synchronization with Inngest and define User database schema

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,18 @@ import express from "express";
 import { ENV } from "./lib/env.js";
 import path from "path";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
+import { inngest } from "./lib/inngest.js";
 
 const app = express();
 
 const __dirname = path.resolve();
+
+// Middelware
+app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use("/api/inngest", serve({client: inngest,functions}));
+
 
 app.get("/", (req, res) => {
   res.status(200).json({
