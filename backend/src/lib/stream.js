e994da8b21a6b023c9f089a08270e0b931d823a6
prashant indurkar <prashantri2000@gmail.com>
2026-06-09 15:22:06 +0530

    feat: implement session management with video and chat integration using Stream SDK

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,5 +1,5 @@
 import { StreamChat } from "stream-chat";
-// import { StreamClient } from "@stream-io/node-sdk";
+import { StreamClient } from "@stream-io/node-sdk";
 import { ENV } from "./env.js";
 
 const apiKey = ENV.STREAM_API_KEY;
@@ -9,9 +9,8 @@ if (!apiKey || !apiSecret) {
   console.error("STREAM_API_KEY or STREAM_API_SECRET is missing");
 }
 
+export const streamClient = new StreamClient(apiKey, apiSecret); // will be used for video calls
 export const chatClient = StreamChat.getInstance(apiKey, apiSecret); // will be used chat features
-
-// export const streamClient = new StreamClient(apiKey, apiSecret); // will be used for video calls
 
 export const upsertStreamUser = async (userData) => {
   try {
@@ -30,5 +29,3 @@ export const deleteStreamUser = async (userId) => {
     console.error("Error deleting the Stream user:", error);
   }
 };
-
-// TODO: add another method to generateToken
--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,9 @@ import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 import { functions, inngest } from "./lib/inngest.js";
 import { keepAliveRenderCron } from "./lib/cron.js";
-import { chatRoutes } from "./routes/chatRoutes.js";
+
+import chatRoutes from "./routes/chatRoutes.js";
+import sessionRoutes from "./routes/sessionRoute.js";
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
@@ -19,6 +21,7 @@ app.use(express.json());
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/chat", chatRoutes);
+app.use("/api/sessions", chatRoutes);
 
 app.get("/health", (_req, res) => {
   res.json({ ok: true });
